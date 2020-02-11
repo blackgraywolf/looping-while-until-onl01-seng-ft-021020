afa4875b370b190702a2776817efda73af7b105b
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-11 23:38:09 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,8 +1,9 @@
 def using_while
   levitation_force = 6
-	while levitation_force < 10 
+	while levitation_force < 11
 	puts "Wingardium Leviosa"
 	levitation_force++
+	break if levitation_force=10
 	end
   #your code here
 end
